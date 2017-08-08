eb601fe5cb74de30f9951b418a5663a00fecc8cc
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-08 21:39:50 +0700

    Change Navbar to NavbarTop

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import 'containers/App.css';
-import Navbar from 'components/Navbar'
+import NavbarTop from 'components/NavbarTop'
 import AppBody from 'components/AppBody'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar></Navbar>
+        <NavbarTop></NavbarTop>
         <AppBody></AppBody>
       </div>
     );
---
+++ src/components/NavbarTop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class NavbarTop extends Component{
+	render(){
+		return(
+			<div id="NavbarTop">
+				<div>ka0</div>
+				<div></div>
+			</div>
+		);
+	}
+}
+
+export default NavbarTop;
