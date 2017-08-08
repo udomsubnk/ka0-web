c1d373565551eccb49375dd70c55474cdee00cdb
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-08 17:47:12 +0700

    Add AppBody and Navbar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import 'containers/App.css';
+import Navbar from 'components/Navbar'
+import AppBody from 'components/AppBody'
 
 class App extends Component {
   render() {
     return (
-
+      <div>
+        <Navbar></Navbar>
+        <AppBody></AppBody>
+      </div>
     );
   }
 }
---
+++ src/components/AppBody.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class AppBody extends Component{
+	render(){
+		return(
+			<div id="AppBody">
+				AppBody
+			</div>
+		);
+	}
+}
+
+export default AppBody;
