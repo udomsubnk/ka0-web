b8aa654d5aaad8455688dde1ea694146497b1cfe
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-14 19:13:05 +0700

    Show close button at right-top when click on logo-clock

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -3,21 +3,26 @@ class NavbarTop extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
-			menuClick : false
+			isMenuClick : false
 		}
 		this.toggleMenu = this.toggleMenu.bind(this);
 	}
+
 	toggleMenu(){
 		this.setState((prevState)=>({
-			menuClick : !prevState.menuClick
+			isMenuClick : !prevState.isMenuClick
 		}));
 	}
+	
 	render(){
 		return(
 			<div id="NavbarTop">
 				<div className="logo-block" onClick={this.toggleMenu}>
 					<div className="logo">I&apos;m Ka0</div>
-					<i className={this.state.menuClick?"fa fa-caret-up":'fa fa-caret-down'}></i>
+					<i className={this.state.isMenuClick?'hide':'fa fa-caret-down'}></i>
+				</div>
+				<div className={this.state.isMenuClick?'close-button':'hide'} onClick={this.toggleMenu}>
+					<i className="fa fa-times"></i>
 				</div>
 			</div>
 		);
