3923ec9af8fde7781673e1ea6c596318cc7eb6b9
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-14 18:54:32 +0700

    Add toggleMenu state and toggle when click at logo-clock

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 class NavbarTop extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuClick : false
+		}
+		this.toggleMenu = this.toggleMenu.bind(this);
+	}
+	toggleMenu(){
+		this.setState((prevState)=>({
+			menuClick : !prevState.menuClick
+		}));
+	}
 	render(){
 		return(
 			<div id="NavbarTop">
-				<div className="logo-block">
+				<div className="logo-block" onClick={this.toggleMenu}>
 					<div className="logo">I&apos;m Ka0</div>
-					<i className="fa fa-caret-down"></i>
+					<i className={this.state.menuClick?"fa fa-caret-up":'fa fa-caret-down'}></i>
 				</div>
 			</div>
 		);
 	}
 }
-
 export default NavbarTop;
