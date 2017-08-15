c3ccf0381bb64f9e9a1e84cdda9d72d44d1888b1
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-15 16:31:35 +0700

    Don't Toggle down-symbol when open the NavFull

--- src/components/NavbarTop.js
+++ src/components/NavbarTop.js
@@ -19,7 +19,7 @@ class NavbarTop extends Component{
 			<div id="NavbarTop" className="hidden-md-up flex-all-center flex-direct-column">
 				<div className="logo-block flex-all-center flex-direct-column" onClick={this.toggleMenu}>
 					<div className="logo">I&apos;m Ka0</div>
-					<i className={this.state.isMenuClick?'fa':'fa fa-caret-down'}></i>
+					<i className='fa fa-caret-down'></i>
 				</div>
 			</div>
 		);
