2223dda75435cbe72f3dcae6493773da30ddc6e7
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-15 16:30:10 +0700

    Close NavFull when click on close-button

--- src/components/NavFull.js
+++ src/components/NavFull.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 class NavFull extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			isNavFullOpened : true
+		}
+		this.toggleNavFull = this.toggleNavFull.bind(this);
+	}
+
+	toggleNavFull(){
+		this.setState((prevState)=>({
+			isNavFullOpened : !prevState.isNavFullOpened
+		}));
+	}
+
 	render(){
 		return (
-			<div id="NavFull">
-				<div className="close-button flex-all-center">
+			<div id="NavFull" className={this.state.isNavFullOpened?'':'hide'}>
+				<div className="close-button flex-all-center" onClick={this.toggleNavFull}>
 					<i className="fa fa-times"></i>
 				</div>
 			</div>
