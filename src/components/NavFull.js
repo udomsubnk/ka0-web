a1b5e9d59dad3c7a23346b3a33d1cb2fe2f5030b
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-08-15 19:33:19 +0700

    Remove close-button

--- src/components/NavFull.js
+++ src/components/NavFull.js
@@ -3,9 +3,7 @@ class NavFull extends Component{
 	render(){
 		return (
 			<div id="NavFull" hidden={!this.props.isMenuClick}>
-				<div className="close-button flex-all-center" onClick={this.props.toggleMenu}>
-					<i className="fa fa-times"></i>
-				</div>
+				
 			</div>
 		);
 	}
