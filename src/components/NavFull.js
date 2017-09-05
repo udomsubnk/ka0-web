13270c25a6fbea3bada58341247d705082b4634c
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-09-05 22:17:40 +0700

    Fill profile detail in NavFull

--- src/components/NavFull.js
+++ src/components/NavFull.js
@@ -5,7 +5,10 @@ class NavFull extends Component{
 			<div id="NavFull" hidden={!this.props.isMenuClick} className="flex-all-center flex-direct-column hidden-md-up">
 				<div className="profile flex-all-center flex-direct-column">
 					<div className="avatar"></div>
-					<div className="detail"></div>
+					<div className="detail">
+						<section className="uppercase white">udomsub naikorn [ kao ]</section>
+						<section className="mgt-5px">Learner/Developer</section>
+					</div>
 				</div>
 				<div className="menu"></div>
 
